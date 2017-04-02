09ee4d18b528bf8c873e62fb3c6fac0e26f79c83
ancutachis <alicornea@gmail.com>
2017-04-03 00:32:54 +0300

    First commit

---
+++ routes/products.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Product = require('../models/Product.js');
+/* GET /products listing. */
+router.get('/', function(req, res, next) {
+  Product.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* POST /products */
+router.post('/', function(req, res, next) {
+  Product.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /products/id */
+router.get('/:id', function(req, res, next) {
+  Product.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
+
