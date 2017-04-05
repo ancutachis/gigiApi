f94b4f3304689d45cc6b0b40cfe83b1f2d462253
ancutachis <alicornea@gmail.com>
2017-04-05 22:08:45 +0300

    Product schema changes

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,18 @@
 var mongoose = require('mongoose');
 var ProductSchema = new mongoose.Schema({
-	id: Number,
+  id: Number,
+  timeStamp : { type: Date, default: Date.now }, 
 	title: String,
-  	description: String, 
-  	imagePath: String, 
-  	category: String, 
-  	createdBy: String, 
-	createDate : { type: Date, default: Date.now }, 
-  	likes: Number, 
-  	dislikes: Number, 
-  	wished: Number, 
-  	retailerName: String, 
-  	link: String, 
-  	cover: Boolean, 
-  	galleryId: Number
+  description: String, 
+  imagePath: String, 
+	category: String, 
+ 	addedBy: String, 
+ 	likes: Number, 
+	dislikes: Number, 
+ 	wished: Number, 
+ 	retailerName: String, 
+	link: String, 
+ 	cover: Boolean, 
+ 	galleryId: Number
 });
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Products', ProductSchema);
